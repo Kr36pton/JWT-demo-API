50ac7ada1ed0e05f89b06acaa2d273f16f653a4a
Kr36pton <jmmesa37@misena.edu.co>
2022-06-02 14:47:45 -0500

    Conexion DataBase, dependencias y Refactorizacion User

--- src/main/java/com/DEMOJWT/demo/dto/User.java
+++ src/main/java/com/DEMOJWT/demo/dto/User.java
@@ -1,30 +1,35 @@
 package com.DEMOJWT.demo.dto;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import javax.validation.constraints.NotNull;
+
+@Document(collection = "users")
 public class User {
+
+    @Id
+    private Integer id;
+
+    @NotNull
     private String user;
+    @NotNull
     private String pwd;
     private String token;
-
     public String getUser() {
         return user;
     }
-
     public void setUser(String user) {
         this.user = user;
     }
-
     public String getPwd() {
         return pwd;
     }
-
     public void setPwd(String pwd) {
         this.pwd = pwd;
     }
-
     public String getToken() {
         return token;
     }
-
     public void setToken(String token) {
         this.token = token;
     }

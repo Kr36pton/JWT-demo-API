b8fb7a16fcdf2f20f9b146e30c89c09c30da5671
Kr36pton <jmmesa37@misena.edu.co>
2022-06-02 15:05:07 -0500

    Creado UserRepository

---
+++ src/main/java/com/DEMOJWT/demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.DEMOJWT.demo.repository;
+
+import com.DEMOJWT.demo.dto.User;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UserRepository extends ReactiveMongoRepository<User, Integer> {
+
+}
